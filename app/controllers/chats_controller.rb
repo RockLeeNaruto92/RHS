d440342fe2c9bd070fc5df7ff7f5a809bfef063e
Bui Minh Thu <buiminhthuk55@gmail.com>
2015-07-21 02:55:47 -0500

    fix chat box interface ok

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,19 +1,26 @@
 class ChatsController < WebsocketRails::BaseController
   def client_connected
-    broadcast_message :new_message,
-      {user: current_user.nil? ? nil : current_user.name,
-        message: t("chat.connected"), is_signed_in: user_signed_in?}
+    unless current_user.nil?
+      broadcast_message :new_message,
+        {user: {id: current_user.id, name: current_user.name},
+          message: {type: "connected", content: t("chat.connected")},
+          is_signed_in: user_signed_in?}
+    end
   end
 
   def client_disconnected
-    broadcast_message :new_message,
-      {user: current_user.nil? ? nil : current_user.name,
-        message: t("chat.disconnected"), is_signed_in: user_signed_in?}
+    unless current_user.nil?
+      broadcast_message :client_disconnected,
+        {user: {id: current_user.id, name: current_user.name},
+          message: {type: "disconnected", content: t("chat.disconnected")},
+          is_signed_in: user_signed_in?}
+    end
   end
 
   def incoming_message
     broadcast_message :new_message,
-      {user: current_user.nil? ? nil : current_user.name,
-        message: message, is_signed_in: user_signed_in?}
+      {user: {id: current_user.id, name: current_user.name},
+        message: {type: "new_message", content: message},
+        is_signed_in: user_signed_in?}
   end
 end
